e07e9b60fb469e522db7d2baf5d7bb6c013b0309
aslah <aslahcholasseri@gmail.com>
2024-10-11 10:55:51 +0530

    ssl

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://51.21.146.181"; // Set your base URL here
+const API_URL = "https://51.21.146.181"; // Set your base URL here
 const axiosInstance = axios.create({ baseURL: API_URL });
 
 // Register user
